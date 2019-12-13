1436895250cb31f3e9d40e9fe9b9e9f32c95e15a
gabschlam <gabschlam@gmail.com>
2019-12-13 15:07:37 -0600

    Delete .DS_Store

--- ProgramacionAvanzada/Assignment 8/bank_codes.h
+++
@@ -1,5 +0,0 @@
-// The different types of operations available
-typedef enum valid_operations {CHECK, DEPOSIT, WITHDRAW, TRANSFER, EXIT} operation_t;
-
-// The types of responses available
-typedef enum valid_responses {OK, INSUFFICIENT, NO_ACCOUNT, BYE, ERROR} response_t;
